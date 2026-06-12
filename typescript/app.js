fc6fa84e1f05a870ac9687c88f911c8f2d047c04
Shreyashj9600 <139689092+Shreyashj9600@users.noreply.github.com>
2026-06-13 00:36:03 +0530

    typescript

---
+++ typescript/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 10;
+var b = 'abc';
+function abc() {
+    console.log('hello world ');
+}
+//# sourceMappingURL=app.js.map
---
+++ typescript/app.ts
@@ -0,0 +1,6 @@
+var a = 10
+var b = 'abc'
+
+function abc() {
+    console.log('hello world !')
+}   
